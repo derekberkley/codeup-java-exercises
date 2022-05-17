70d226703f7543b72141e8fa3ae4956f4e8888fb
Derek <99042983+derekberkley@users.noreply.github.com>
2022-05-17 13:42:19 -0500

    completed control IO exercise

--- ConsoleExercises.java
+++ ConsoleExercises.java
@@ -34,11 +34,25 @@ public class ConsoleExercises {
         System.out.print(System.lineSeparator());
 
 //  TODO: Prompt a user to enter a sentence, then store that sentence in a String variable using the next
-//   method. Then, display that sentence back to the user.
+//   method. Then, display that sentence back to the user.  Then, rewrite the above example using the nextLine method.
 
         System.out.println("Enter a sentence: ");
         String userSentence = userInt.nextLine();
         System.out.printf("Your sentence is: \"%s\"", userSentence); //only captures the first word
+
+//  TODO: Prompt the user to enter values of length and width of a classroom at Codeup (or your room if virtual).
+//      Use the nextLine method each time you need to get user input. In this case, we need it twice, once to get
+//          the user input for the length and again to get the user input for the width. Parse the resulting strings to a numeric type.
+//      Assume that the rooms are perfect rectangles.
+//      Assume that the user will enter valid numeric data for length and width.
+
+            System.out.println("What is the length of your room in feet?");
+            String roomLength = userInt.next();
+            System.out.println("What is the width of your room in feet?");
+            String roomWidth = userInt.next();
+            Double roomPer = Double.parseDouble(roomWidth) * 2 + Double.parseDouble(roomLength) * 2;
+            Double roomArea = Double.parseDouble(roomWidth) * Double.parseDouble(roomLength);
+            System.out.printf("Your %s by %s foot room has a perimeter of %s ft and an area of %s sq. ft.", roomWidth, roomLength, roomPer, roomArea);
 
 
     }
