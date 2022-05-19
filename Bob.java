732b4d57ea7277b0afd445541d2d620f2dac7e51
Derek <99042983+derekberkley@users.noreply.github.com>
2022-05-19 15:29:59 -0500

    completed String Comparison (Bob) exercise

--- Bob.java
+++ Bob.java
@@ -1,36 +1,54 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Bob {
 
     public static void main (String[] args) {
-        System.out.println("Hi, I'm Bob.");
-        talktoBob();
 
-
-
-
-
-    }  // End psvm
-
-    public static void talktoBob() {
         Scanner scanner = new Scanner(System.in);
 
-        String userInput = scanner.nextLine();
+        System.out.println("Hi, I'm Bob.");
+        String userInput;
 
-        if (userInput.endsWith("?")) {
-            System.out.println("sure.");
-            talktoBob();
-        } else if (userInput.endsWith("!")) {
-            System.out.println("whoa, chill out.");
-            talktoBob();
-        } else if (userInput.equals("")) {
-            System.out.println("fine, be that way.");
-            talktoBob();
-        } else {
-            System.out.println("whatever");
-            talktoBob();
-        }
-        return;
-    };
+        do {
+            userInput = scanner.nextLine();
+            if (userInput.endsWith("?")) {
+                System.out.println("sure.");
+            } else if (userInput.endsWith("!")) {
+                System.out.println("whoa, chill out.");
+            } else if (userInput.equals("")) {
+                System.out.println("fine, be that way.");
+            } else {
+                System.out.println("whatever");
+            }
+            System.out.println("userInput before conditional: " + userInput);
+        } while (!userInput.equals("bye"));
+//        Sting is NOT a data type. must use .equals to compare strings since it is a method
+
+        System.out.println("finally. bye.");
+
+    };  // End psvm
+
+
+//    public static void talktoBob() {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        String userInput = scanner.nextLine();
+//
+//        if (userInput.endsWith("?")) {
+//            System.out.println("sure.");
+//            talktoBob();
+//        } else if (userInput.endsWith("!")) {
+//            System.out.println("whoa, chill out.");
+//            talktoBob();
+//        } else if (userInput.equals("")) {
+//            System.out.println("fine, be that way.");
+//            talktoBob();
+//        } else {
+//            System.out.println("whatever");
+//            talktoBob();
+//        }
+//        return;
+//    };
 
 } //End Public Class Bob
