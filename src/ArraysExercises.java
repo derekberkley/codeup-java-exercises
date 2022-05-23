c0ce9b7b8b5390320dd15aa6a17c59822cad0f95
Derek <99042983+derekberkley@users.noreply.github.com>
2022-05-23 11:11:44 -0500

    arrays lecture notes and practice

---
+++ src/ArraysExercises.java
@@ -0,0 +1,21 @@
+package src;
+import java.io.*;
+import java.util.*;
+
+public class ArraysExercises {
+
+    public static void main(String[] args) {
+
+        String[] practice = {"coffee", "water", "soda", "juice", "milk"};
+
+//        Either need to initialize the size of the array and add elements 1 by 1 OR initialize w/ all elements
+//        String[] weatherTypes = new String[5];
+//        weatherTypes[] = {"sunny", "cloudy", "overcast", "rainy", "windy"};
+
+        long[] dailySteps = {7543, 10001, 5245, 8978, 6767, 9876, 10210};
+        System.out.println(dailySteps);
+        System.out.println(Arrays.toString(dailySteps));
+
+    } //end of psvm
+
+} //end of ArraysExercises Class
