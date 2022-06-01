487ae16b8dd414b70100571157de8305878d142b
Derek <99042983+derekberkley@users.noreply.github.com>
2022-06-01 09:23:50 -0500

    adding java warmup

--- src/Practice.java
+++ src/Practice.java
@@ -11,7 +11,11 @@ public class Practice {
 //        changeString(changeMe);
 //
 //        System.out.println(changeMe);
-        diceRoll();
+//        diceRoll();
+        inDays(33);
+        inDays(65);
+        doMath(3, "*", 4);
+        doMath(18, "/", 3);
 
     }
 // This doesn't have a return statement, it doesn't permanently change the variable.
@@ -21,37 +25,64 @@ public class Practice {
 //        s = "Wub a lub a dub dub";
 //    } //End changeString
 
-    //TODO: Create an application that simulates dice rolling.
-    public static void diceRoll() {
-        Scanner sc = new Scanner(System.in);
+//    //TODO: Create an application that simulates dice rolling.
+//    public static void diceRoll() {
+//        Scanner sc = new Scanner(System.in);
+//
+//        //TODO: Ask the user to enter the number of sides for a pair of dice.
+//        System.out.println("How many sides are on your dice?");
+//        int sides = sc.nextInt();
+//
+//        //TODO: Prompt the user to roll the dice.
+//        System.out.println("Great! Do you want to roll your pair of dice with " + sides + " sides? (y/n)");
+//        String doRoll = sc.next();
+//        if (doRoll.equals("n")) {
+//            System.out.println("fine, keep your dice. bye.");
+//            return;
+//        }
+//        //TODO: "Roll" two n-sided dice, display the results of each, and then ask the user if
+//        // he/she wants to roll the dice again. Use static methods to implement the method(s) that generate
+//        // the random numbers. Use the .random method of the java.lang.Math class to generate random numbers.
+//        Random rand = new Random();
+//        int upperbound = sides;
+//        int dice1 = rand.nextInt(upperbound);
+//        int dice2 = rand.nextInt(upperbound);
+//        System.out.println("You rolled a " + dice1 + " and " + dice2);
+//
+//        System.out.println("Nice roll! Do you want to roll again? (y/n)");
+//        doRoll = sc.next();
+//        if (doRoll.equals("n")) {
+//            System.out.println("fine, keep your dice. bye.");
+//            return;
+//        }
+//
+//    } //end diceRoll
 
-        //TODO: Ask the user to enter the number of sides for a pair of dice.
-        System.out.println("How many sides are on your dice?");
-        int sides = sc.nextInt();
+    //TODO:  Create a function that takes the age in years and returns the age in days.
+    //example input: 65
+    //expected output: 23725
 
-        //TODO: Prompt the user to roll the dice.
-        System.out.println("Great! Do you want to roll your pair of dice with " + sides + " sides? (y/n)");
-        String doRoll = sc.next();
-        if (doRoll.equals("n")) {
-            System.out.println("fine, keep your dice. bye.");
-            return;
+    public static int inDays(int x) {
+        int result = x * 365;
+        System.out.println("age (yrs): " + x + ", age (days): " + result);
+        return result;
+    }
+
+    public static int doMath(int x, String y, int z) {
+        int result = 0;
+        if (y.equals("/")) {
+            result = x / z;
+        } else if (y.equals("*")) {
+            result = x * z;
+        } else if (y.equals("+")) {
+            result = x + z;
+        } else if (y.equals("-")) {
+            result = x - z;
+        } else {
+            System.out.println("I do not recognize that operator.");
         }
-        //TODO: "Roll" two n-sided dice, display the results of each, and then ask the user if
-        // he/she wants to roll the dice again. Use static methods to implement the method(s) that generate
-        // the random numbers. Use the .random method of the java.lang.Math class to generate random numbers.
-        Random rand = new Random();
-        int upperbound = sides;
-        int dice1 = rand.nextInt(upperbound);
-        int dice2 = rand.nextInt(upperbound);
-        System.out.println("You rolled a " + dice1 + " and " + dice2);
-
-        System.out.println("Nice roll! Do you want to roll again? (y/n)");
-        doRoll = sc.next();
-        if (doRoll.equals("n")) {
-            System.out.println("fine, keep your dice. bye.");
-            return;
-        }
-
+        System.out.println(x + " " + y + " " + z + " = " + result);
+        return result;
     }
 
 
